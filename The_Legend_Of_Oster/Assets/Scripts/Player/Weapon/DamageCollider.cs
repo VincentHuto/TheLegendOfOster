081c61c9785c2656926839d5d9cc7892b0aa6d01
Vincent Huto <amae804@gmail.com>
2021-11-27 23:29:26 -0600

    Added Recorder Weapon

--- The_Legend_Of_Oster/Assets/Scripts/Player/Weapon/DamageCollider.cs
+++ The_Legend_Of_Oster/Assets/Scripts/Player/Weapon/DamageCollider.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DamageCollider : MonoBehaviour
 {
     Collider damageCollider;
+    //Make sure to set this within the prefab so it propagates to all instances of this weapon
     public int currentWeaponDamage = 25;
 
     private void Awake()
@@ -26,19 +27,18 @@ public class DamageCollider : MonoBehaviour
 
     private void OnTriggerEnter(Collider colllision)
     {
-        if(colllision.tag == "Player")
+        if (colllision.tag == "Player")
         {
-           PlayerStats playerStats =  colllision.GetComponentInParent<PlayerStats>();
-            if(playerStats != null)
+            PlayerStats playerStats = colllision.GetComponentInParent<PlayerStats>();
+            if (playerStats != null)
             {
                 playerStats.TakeDamage(currentWeaponDamage);
             }
         }
         //TAG THE COLLIDER AS ENEMY NOT THE PREFAB, SAME AS PLAYER
-        if(colllision.tag == "Enemy")
+        if (colllision.tag == "Enemy")
         {
             EnemyStats enemyStats = colllision.GetComponentInParent<EnemyStats>();
-            Debug.Log("t");
             if (enemyStats != null)
             {
                 enemyStats.TakeDamage(currentWeaponDamage);
