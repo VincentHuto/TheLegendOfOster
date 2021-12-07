87e10853a64f43618e7e1fc3ca5f49fd8088fb97
Flare26 <47835828+Flare26@users.noreply.github.com>
2021-12-06 20:49:29 -0600

    Baked Lightmap settings

---
+++ The_Legend_Of_Oster/Assets/Scripts/Disco.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disco : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward);
+    }
+}
---
+++ The_Legend_Of_Oster/Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] AudioClip[] deathclips;
+    [SerializeField] AudioClip[] voiceclips;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
