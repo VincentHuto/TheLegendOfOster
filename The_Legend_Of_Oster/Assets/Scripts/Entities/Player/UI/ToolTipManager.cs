60502756db23090846a5f6fb8616e35c8cb10dc8
Vincent Huto <amae804@gmail.com>
2021-12-08 20:55:58 -0600

    Tool tips now function and disapper when neccisary

--- The_Legend_Of_Oster/Assets/Scripts/Entities/Player/UI/ToolTipManager.cs
+++ The_Legend_Of_Oster/Assets/Scripts/Entities/Player/UI/ToolTipManager.cs
@@ -3,15 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(EventTrigger))]
-public class MouseInputUIBlocker : MonoBehaviour
+public class ToolTipManager : MonoBehaviour
 {
-    public static bool BlockedByUI;
+    public bool BlockedByUI;
     private EventTrigger eventTrigger;
+    Image icon;
+    public ItemDescriptionWindow descriptionWindow;
 
     private void Start()
     {
+        icon = GetComponentInChildren<Image>();
+
         eventTrigger = GetComponent<EventTrigger>();
         if (eventTrigger != null)
         {
@@ -20,7 +25,15 @@ public class MouseInputUIBlocker : MonoBehaviour
             enterUIEntry.eventID = EventTriggerType.PointerEnter;
             enterUIEntry.callback.AddListener((eventData) => { EnterUI(); });
             eventTrigger.triggers.Add(enterUIEntry);
+            if (eventTrigger.gameObject.GetComponentInChildren<Image>().sprite != null && descriptionWindow.GetComponentInChildren<Image>() != null)
+            {
 
+                Text descText = descriptionWindow.gameObject.GetComponentInChildren<Text>();
+                Image descImage = descriptionWindow.gameObject.GetComponentInChildren<Image>();
+                ItemStack hoveredStack = eventTrigger.gameObject.GetComponent<InventorySlot>().item;
+                descImage.sprite =hoveredStack.itemType.itemIcon;
+                descText.text =hoveredStack.itemType.name+"\n" + hoveredStack.itemType.itemDesc;
+            }
             //Pointer Exit
             EventTrigger.Entry exitUIEntry = new EventTrigger.Entry();
             exitUIEntry.eventID = EventTriggerType.PointerExit;
